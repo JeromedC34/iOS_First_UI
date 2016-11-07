2ad3e04db4a19d0e67e2f277fe8e8b60789b8bb3
Jerome <jdc@free.fr>
2016-11-07 15:34:01 +0100

    Initial Commit

---
+++ iOS_First_UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_First_UI
+//
+//  Created by imac on 07/11/2016.
+//  Copyright © 2016 imac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
