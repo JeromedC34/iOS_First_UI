1ef3a49835a62f3dbcf6e693715c4fdd60bb62cc
Jerome <jdc@free.fr>
2016-11-09 09:27:07 +0100

    Add constraints / UI

--- iOS_First_UI/ViewController.swift
+++ iOS_First_UI/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var displayText: UILabel!
+    @IBAction func clickButton(_ sender: UIButton) {
+        displayText.text = "Bonjour \(inputText.text!) !"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
